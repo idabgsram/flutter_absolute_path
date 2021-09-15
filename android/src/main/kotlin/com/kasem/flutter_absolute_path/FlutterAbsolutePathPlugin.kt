18a7efb3bb3f6a98268dee1856f7790a4d6ccbb1
Idabgsram <idabgsram@gmail.com>
2021-09-15 12:49:34 +0800

    update v2 embedding

--- android/src/main/kotlin/com/kasem/flutter_absolute_path/FlutterAbsolutePathPlugin.kt
+++ android/src/main/kotlin/com/kasem/flutter_absolute_path/FlutterAbsolutePathPlugin.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Environment
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.util.Log
 import android.content.pm.ProviderInfo
@@ -16,40 +17,46 @@ import android.content.pm.PackageInfo
 import java.security.Provider
 
 
-class FlutterAbsolutePathPlugin(private val context: Context) : MethodCallHandler {
+public class FlutterAbsolutePathPlugin: FlutterPlugin, MethodCallHandler {
+
+    private var channel : MethodChannel? = null
+    private lateinit var context: Context
 
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "flutter_absolute_path")
-            channel.setMethodCallHandler(FlutterAbsolutePathPlugin(registrar.context()))
+          val plugin = FlutterAbsolutePathPlugin()
+          plugin.setupChannel(registrar.messenger(), registrar.context())
         }
-    }
+      }
 
-    override fun onMethodCall(call: MethodCall, result: Result) {
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        setupChannel(binding.binaryMessenger, binding.applicationContext)
+        context = binding.applicationContext
+      }
+    
+      override fun onDetachedFromEngine(p0: FlutterPlugin.FlutterPluginBinding) {
+        teardownChannel();
+      }
+    
+      fun setupChannel(messenger: BinaryMessenger, context: Context) {
+        channel = MethodChannel(messenger, "flutter_absolute_path")
+        channel?.setMethodCallHandler(this)
+      }
+    
+      private fun teardownChannel() {
+        channel?.setMethodCallHandler(null)
+        channel = null
+      }
+
+      override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when {
             call.method == "getAbsolutePath" -> {
                 val uriString = call.argument<Any>("uri") as String
                 val uri = Uri.parse(uriString)
-
-//                val provider = applicationProviders?.firstOrNull { uri.authority == it.authority }
-//                if (provider != null) {
-//                    val folderPath = Environment.getExternalStorageDirectory().path + "/Pictures"
-//                    result.success("$folderPath/${uri.lastPathSegment}")
-//                    return
-//                }
-
                 result.success(FileDirectory.getAbsolutePath(this.context, uri))
             }
             else -> result.notImplemented()
         }
     }
-
-//    val applicationProviders: List<ProviderInfo>? by lazy {
-//        val applicationId = context.packageName
-//        context.packageManager
-//                .getInstalledPackages(PackageManager.GET_PROVIDERS)
-//                .firstOrNull { it.packageName == applicationId }
-//                ?.providers?.toList()
-//    }
 }
